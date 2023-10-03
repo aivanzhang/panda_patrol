349561c24b1a534c9d6329d4636adbb278abdeb3
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-10-03 16:48:16 -0700

    add bash flag for prod

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+# Default repository
+REPO="testpypi"
+
+# Parse flags
+while getopts "p" flag; do
+    case "${flag}" in
+        p) REPO="pypi";;
+    esac
+done
+
 python3 -m build &&
 twine check dist/* &&
-python3 -m twine upload --config-file .pypirc --repository pypi dist/* 
+python3 -m twine upload --config-file .pypirc --repository $REPO dist/* 
--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,1 +1,13 @@
-python3 -m twine upload --repository testpypi dist/* --config-file .pypirc
+#!/bin/bash
+
+# Default repository
+REPO="testpypi"
+
+# Parse flags
+while getopts "p" flag; do
+    case "${flag}" in
+        p) REPO="pypi";;
+    esac
+done
+
+python3 -m twine upload --repository $REPO dist/* --config-file .pypirc
