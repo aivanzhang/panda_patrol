f267b95ea3e1d3c82359554ba724874c55a2302a
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-10-22 22:08:10 -0700

    fixes

--- panda_patrol/dbt/find_dbt_target_results.py
+++ panda_patrol/dbt/find_dbt_target_results.py
@@ -1,12 +1,16 @@
 import os
+from pathlib import Path
 
 
 def find_dbt_target_results():
-    target_results_path = os.path.join(
-        os.getcwd() + "/examples/dbt_pipeline", "target/run_results.json"
-    )
+    target_results_path = os.path.join(os.getcwd(), "target/run_results.json")
 
     if os.path.exists(target_results_path):
         return target_results_path
+    else:
+        target_directory = os.path.join(os.getcwd(), "target")
+        if not os.path.exists(target_directory):
+            os.mkdir(target_directory)
+        Path(os.path.join(target_directory, "run_results.json")).touch()
 
     return None
