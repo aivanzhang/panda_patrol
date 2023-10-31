afc4b850ca161b3913495d81a96edfe32b467cd9
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-10-30 21:07:59 -0700

    rm pandas dep

--- examples/airflow/install.sh
+++ examples/airflow/install.sh
@@ -8,5 +8,5 @@ PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 # For example this would install 2.7.2 with python 3.8: https://raw.githubusercontent.com/apache/airflow/constraints-2.7.2/constraints-3.8.txt
 
-pip install panda-patrol ydata_profiling pandas
+pip install panda-patrol ydata_profiling
 pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
